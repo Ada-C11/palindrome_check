a5a3bc74685a287cb44578093eaff3abaec2d4f7
Shubha Rajan <shubharajan94@gmail.com>
2019-03-27 18:07:26 -0700

    working method checks if phrase is palindrome and ignores whitespace. passes all tests.

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,7 +1,39 @@
+require "pry"
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n is the length of the phrase. While there are nested loops,
+# they use the same iterator as their containing loop
+# Space complexity: O(1), variable creation was not proportional to the number of characters in the string.
 def palindrome_check(my_phrase)
-  raise NotImplementedError
+  if my_phrase
+    index = 0
+    rindex = my_phrase.length - 1
+    # continue to compare characters in front to characters in back until all characters have been compared at least once
+    while index < rindex
+      front_char = my_phrase[index]
+      back_char = my_phrase[rindex]
+      # seek next non-whitespace character from front
+      if front_char == " " || front_char == "\t" || front_char == "\n"
+        until front_char != " " && front_char != "\t" && front_char != "\n"
+          index += 1
+          front_char = my_phrase[index]
+        end
+      end
+      # seek next non-whitespace character from back
+      if back_char == " " || back_char == "\t" || back_char == "\n"
+        until back_char != " " && back_char != "\t" && back_char != "\n"
+          rindex -= 1
+          back_char = my_phrase[rindex]
+        end
+      end
+      # compare characters
+      return false if front_char != back_char
+      # increment indices
+      index += 1
+      rindex -= 1
+    end
+    return true
+  else
+    return false
+  end
 end
