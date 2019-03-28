d93a4c8dfd05a2893c55e5886f7e501883dafecd
Angela <angela.ohh@gmail.com>
2019-03-28 11:53:10 -0700

    added spaces check in main loop

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -3,25 +3,17 @@
 # Time complexity: O(n) where n is the size of my_phrase
 # Space complexity: O(1)
 def palindrome_check(my_phrase)
-  # temp = my_phrase
-  if my_phrase == nil
-    return false
-  end
-
-  #eliminate any spaces in phrase
-  tracking_index = 0
-  while tracking_index < my_phrase.length
-    if my_phrase[tracking_index] == " "
-      my_phrase[tracking_index] = ""
-      tracking_index -= 1
-    end
-    tracking_index += 1
-  end
-
+  return false if my_phrase == nil
   # check if each letter is the same
   smaller_index = 0
   larger_index = (my_phrase.length - 1)
   while smaller_index <= larger_index
+    until my_phrase[smaller_index] != " "
+      smaller_index += 1
+    end
+    until my_phrase[larger_index] != " "
+      larger_index -= 1
+    end
     if my_phrase[smaller_index] != my_phrase[larger_index]
       return false
     end
