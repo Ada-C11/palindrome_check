9886a5908a59658a684419806e7da065b82833eb
Grace Shea <grace.m.shea@gmail.com>
2019-03-30 15:22:09 -0700

    initial method written

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -2,6 +2,36 @@
 # Return true if the string is a palindrome. Return false otherwise.
 # Time complexity: ?
 # Space complexity: ?
+
 def palindrome_check(my_phrase)
-  raise NotImplementedError
+  leftindex = 0
+  rightindex = my_phrase.length - 1
+
+  while leftindex < rightindex
+    l_char = my_phrase[leftindex]
+    r_char = my_phrase[rightindex]
+
+    if l_char == " "
+      until l_char != " "
+        leftindex += 1
+        l_char = my_phrase[leftindex]
+      end
+    end
+
+    if r_char == " "
+      until r_char != " "
+        rightindex -= 1
+        r_char = my_phrase[rightindex]
+      end
+    end
+
+    return false if l_char != r_char
+
+    leftindex += 1
+    rightindex -= 1
+  end
+
+  return true
+else
+  return false
 end
