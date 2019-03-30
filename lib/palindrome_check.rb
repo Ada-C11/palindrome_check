7bb64c4e010d16c57f60523b483f0c88aed8c715
Nara <naraa_520@yahoo.com>
2019-03-29 22:29:22 -0700

    Added palindrome_check function with all tests passing

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -3,5 +3,24 @@
 # Time complexity: ?
 # Space complexity: ?
 def palindrome_check(my_phrase)
-  raise NotImplementedError
+  return false if my_phrase == nil
+  i = 0 
+  j = my_phrase.length - 1
+  while i < my_phrase.length
+    while i < my_phrase.length && my_phrase[i] == " "
+      i += 1 
+    end
+    while i < my_phrase.length && my_phrase[j] == " "
+      j -= 1 
+    end
+
+    if my_phrase[j] != " " && my_phrase[i] != " "
+      if my_phrase[i] != my_phrase[j]
+        return false
+      end
+      i += 1
+      j -= 1
+    end
+  end
+  return true
 end
