b86123d4f7eec38c60a64aa17d6e56e3b9d3f4b3
Ariana Bray <aribray@gmail.com>
2019-04-02 08:22:22 -0700

    refactored palindrome_check

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -5,20 +5,24 @@
 def palindrome_check(my_phrase)
   return false if my_phrase.nil?
   
+
   i = 0
   j = my_phrase.length - 1
 
-  while i < j
+  while (i < j)
     if my_phrase[i] == my_phrase[j]
-    i += 1
-    j -= 1
-  elsif my_phrase[i] == " "
-    i += 1
-  elsif my_phrase[j] == " "
-    j -= 1
-  elsif my_phrase[i] != my_phrase[j] && my_phrase[i] != " " && my_phrase[j] != " "
-    return false
+      i += 1
+      j -= 1
+      while my_phrase[j] == " "
+        j -= 1
+      end
+      while my_phrase[i] == " "
+        i += 1
+      end
+    else
+      return false
+    end
   end
-end
+
   return true
 end
