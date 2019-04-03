56a5b55d26c28f330bebf7787137555b2310fa9b
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-04-02 17:20:44 -0700

    Wrote method, passes tests

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -3,5 +3,27 @@
 # Time complexity: ?
 # Space complexity: ?
 def palindrome_check(my_phrase)
-  raise NotImplementedError
+  if my_phrase == ""
+    return true
+  elsif my_phrase == nil
+    return false
+  end
+  i = 0
+  j = my_phrase.length - 1
+  while i < j
+    if my_phrase[i] == " " || my_phrase[j] == " "
+      if my_phrase[i] == " "
+        i += 1
+      end
+      if my_phrase[j] == " "
+        j -= 1
+      end
+    elsif my_phrase[i] != my_phrase[j]
+      return false
+    else
+      i += 1
+      j -= 1
+    end
+  end
+  return true
 end
